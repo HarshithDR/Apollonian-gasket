f379c03f76a7a717a3fc25b92d9c5b567cf7bb5d
Harshith <harshithdr10@gmail.com>
2024-03-17 18:33:05 -0500

    junk file

---
+++ junk.py
@@ -0,0 +1,44 @@
+# import matplotlib.pyplot as plt
+
+# fig, ax = plt.subplots()
+
+# c = (0.5,0.5)
+# r = 0.3
+
+# circle = plt.Circle(c,r,fill = False)
+# ax.add_patch(circle)
+# ax.set_aspect('equal')
+# ax.axis('off')
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+
+# class Circle:
+#     def __init__(self, bend, x, y):
+#         self.x = x
+#         self.y = y
+#         self.bend = bend
+#         self.radius = abs(1 / self.bend)
+
+# c1 = Circle(-1/200, 200, 200)
+# c2 = Circle(1/100, 100, 200)
+# c3 = Circle(1/100, 300, 200)
+
+# fig, ax = plt.subplots()
+
+# circle1 = plt.Circle((c1.x, c1.y), c1.radius, fill=False)
+# circle2 = plt.Circle((c2.x, c2.y), c2.radius, fill=False)
+# circle3 = plt.Circle((c3.x, c3.y), c3.radius, fill=False)
+
+# ax.add_patch(circle1)
+# ax.add_patch(circle2)
+# ax.add_patch(circle3)
+
+# ax.set_aspect('equal')
+# ax.axis('off')
+# ax.margins(0)
+# ax.autoscale()
+# plt.show()
+
+
